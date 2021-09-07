dadb6cf12405668ec854effce32cd98d20702f48
BlishchAndrew <blishch.andrew@gmail.com>
2021-09-07 17:54:28 +0300

    fix header and footer

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,12 +1,12 @@
 const btnShowBlock = document.querySelectorAll('._btn-toggle');
-const closeBlock = document.querySelector('.header-special__btn-close');
+const closeBlock = document.querySelector('.header-special__btn_close');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.header__overlay');
 
 btnShowBlock.forEach(item => {
 	item.addEventListener('click', function (){
-		if (item.classList.contains("show-block")) {
-			item.classList.remove('show-block');
+		if (item.classList.contains("_show-block")) {
+			item.classList.remove('_show-block');
 			overlay.classList.remove('_active');
 		} else {
 			addClass(item);
@@ -16,19 +16,19 @@ btnShowBlock.forEach(item => {
 
 function addClass(currentItem) {
 	btnShowBlock.forEach((item) => {
-		item.classList.remove('show-block');
+		item.classList.remove('_show-block');
 		overlay.classList.remove('_active');
 	})
 	item = currentItem;
-	item.classList.add('show-block');
+	item.classList.add('_show-block');
 	overlay.classList.add('_active');
 }
 
 document.addEventListener('click', function(e){
 	if (!e.target.closest('.hidden-block, ._btn-toggle')) {
 		btnShowBlock.forEach(item => {
-			if (item.classList.contains('show-block')) {
-				item.classList.remove('show-block');
+			if (item.classList.contains('_show-block')) {
+				item.classList.remove('_show-block');
 			}
 		})
 		overlay.classList.remove('_active');
@@ -37,51 +37,30 @@ document.addEventListener('click', function(e){
 
 closeBlock.addEventListener('click', function(e){
 	btnShowBlock.forEach((item) => {
-		item.classList.remove('show-block');
+		item.classList.remove('_show-block');
 	})
 	overlay.classList.remove('_active');
 })
 
-const btnToggleTheme = document.querySelector('.nav-header__toggle-theme');
-const cntTheme = document.querySelector('.nav-header__nav');
-
-btnToggleTheme.addEventListener('click', function (){
-	cntTheme.classList.toggle('_black');
-})
-
-const btnToggleThemeSpecial = document.querySelector('.header-special__toggle-theme');
-const cntThemeSpecial = document.querySelector('.header-special__block');
-
 // const btnShowAccardeon = document.querySelector('.ref-info__btn');
 // const wrapperDrop = document.querySelector('.ref-info-drop');
 // btnShowAccardeon.addEventListener('click', function (){
-// 	btnShowAccardeon.classList.toggle('show-block');
-// 	wrapperDrop.classList.toggle('show-block');
+// 	btnShowAccardeon.classList.toggle('_show-block');
+// 	wrapperDrop.classList.toggle('_show-block');
 // });
 
-btnToggleThemeSpecial.addEventListener('click', function (){
-	cntThemeSpecial.classList.toggle('_black');
+
+// ===========  Toggle theme nav-header ===========================
+$('.nav-header__toggle-theme').click(function (){
+	$('.nav-header__nav').toggleClass('_black');
 })
 
-let tabs = document.querySelectorAll("._tabs");
-for (let index = 0; index < tabs.length; index++) {
-	let tab = tabs[index];
-	let tabs_items = tab.querySelectorAll("._tabs-item");
-	let tabs_blocks = tab.querySelectorAll("._tabs-block");
-	for (let index = 0; index < tabs_items.length; index++) {
-		let tabs_item = tabs_items[index];
-		tabs_item.addEventListener("click", function (e) {
-			for (let index = 0; index < tabs_items.length; index++) {
-				let tabs_item = tabs_items[index];
-				tabs_item.classList.remove('_active');
-				tabs_blocks[index].classList.remove('_active');
-			}
-			tabs_item.classList.add('_active');
-			tabs_blocks[index].classList.add('_active');
-			e.preventDefault();
-		});
-	}
-}
+// ===========  Toggle theme header-special =======================
+$('.header-special__btn_toggle-theme').click(function (){
+	$('.header-special__wrapper').toggleClass('_black');
+})
+
+
 
 
 $(function(){
@@ -153,3 +132,13 @@ $(function(){
 		]
 	});
 });
+
+function ibg() {
+	let ibg = document.querySelectorAll("._ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
