ca4053f7e4c0c53e8313b26571c58dccfcfd9c65
dyamorph <dyamorph22@gmail.com>
2024-08-10 15:05:41 +0300

    add several pages

--- #src/js/files/select-sidebar-elem.js
+++ #src/js/files/select-sidebar-elem.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname;
     const menuItems = document.querySelectorAll('.sidebar__list a');
+    console.log(currentPath);
 
     menuItems.forEach(function(item) {
         let currentHref = "/" + item.getAttribute('href') ;
+        console.log(currentHref);
         if (currentHref === currentPath) {
             item.classList.add('active');
         }
