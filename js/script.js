8307d1280e241b883e774659fc52c75c6cfa29b3
dyamorph <dyamorph22@gmail.com>
2024-08-12 09:54:44 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -3236,9 +3236,11 @@ var Calculator2021 = function (a) {
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname;
     const menuItems = document.querySelectorAll('.sidebar__list a');
+    console.log(currentPath);
 
     menuItems.forEach(function(item) {
         let currentHref = "/" + item.getAttribute('href') ;
+        console.log(currentHref);
         if (currentHref === currentPath) {
             item.classList.add('active');
         }
