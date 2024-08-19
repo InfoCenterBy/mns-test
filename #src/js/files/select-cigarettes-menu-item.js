3c1f2cc5cfe316fc50232c2884c5ecfa7e7f9224
dyamorph <dyamorph22@gmail.com>
2024-08-19 15:08:55 +0300

    minor fixes

--- #src/js/files/select-cigarettes-menu-item.js
+++ #src/js/files/select-cigarettes-menu-item.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const currentPage = window.location.pathname;
   const menuItems = document.querySelectorAll('.cigarettes__menu-item a');
-  console.log(menuItems)
+
   menuItems.forEach(item => {
       const href = "/" + item.getAttribute('href');
       if (href.includes(currentPage)) {
