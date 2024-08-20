cb5b087f1ec25c95b20821ddcac10730cc5c63bc
Dima Skrinnikov <dima.skrinnikov@innovise-group.com>
2024-08-20 17:34:26 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -3250,7 +3250,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("DOMContentLoaded", function() {
   const currentPage = window.location.pathname;
   const menuItems = document.querySelectorAll('.cigarettes__menu-item a');
-  console.log(menuItems)
+
   menuItems.forEach(item => {
       const href = "/" + item.getAttribute('href');
       if (href.includes(currentPage)) {
